b8091ee1b1c391968730b8232296fc8b64540ef6
David Snopek <dsnopek@gmail.com>
2025-08-19 19:07:06 -0500

    Move ScreenContainer work a bit better in the editor

--- demo/addons/sgui/screen_container.gd
+++ demo/addons/sgui/screen_container.gd
@@ -2,14 +2,20 @@
 extends Container
 
 @export var current_screen: String:
+	get:
+		return _current_screen
 	set(v):
-		show_screen(v)
+		if is_node_ready():
+			show_screen(v)
+		else:
+			_current_screen = v
 
+var _current_screen: String
 var _current_screen_control: Control
 var _screens: Array[Control]
 
 func _ready() -> void:
-	_update_children()
+	_update_children(true)
 
 	for child in get_children():
 		if child.has_method('_setup_screen'):
@@ -17,11 +23,15 @@ func _ready() -> void:
 			child.visible = false
 
 	if get_child_count() > 0:
-		show_screen(get_child(0).name)
+		if _current_screen != '':
+			show_screen(_current_screen)
+		else:
+			show_screen(get_child(0).name)
 
 
-func _update_children() -> void:
+func _update_children(p_first := false) -> void:
 	var children := get_children()
+	var show_first_screen: bool = (not p_first and _screens.size() == 0)
 
 	var removed: Array[Control]
 	for screen in _screens:
@@ -29,20 +39,48 @@ func _update_children() -> void:
 			removed.push_back(screen)
 
 	for screen in removed:
-		# @todo If the current screen is removed, then change to the first screen.
-		# @todo Disconnect any signals (need to check if the object is still valid - should we use object ids or weakref?)
+		if screen == _current_screen_control:
+			# In this case, we don't want to call `_hide_screen()`, so just clear it.
+			_current_screen_control = null
+			if get_child_count() > 0:
+				show_screen(get_child(0).name)
+			else:
+				_current_screen = ''
 		_screens.erase(screen)
 
 	for child in children:
 		if not _screens.has(child) and child is Control:
-			# @todo Connect to signal to track name changes
 			_screens.push_back(child)
+
+	if children.size() > 0 and show_first_screen:
+		show_screen(get_child(0).name)
+
+	if children.size() > 0 or removed.size() > 0:
+		notify_property_list_changed()
+
+
+func _validate_property(p_property: Dictionary) -> void:
+	if p_property.name == "current_screen":
+		p_property.hint = PROPERTY_HINT_ENUM_SUGGESTION
+
+		var children = get_children()
+		if children.size() > 0:
+			var names: PackedStringArray
+			for child in children:
+				names.push_back(child.name)
+
+			p_property.hint_string = ",".join(names)
+		else:
+			p_property.hint_string = ''
 
 
 func show_screen(p_name: String, p_info: Dictionary = {}) -> bool:
-	var screen = get_node(p_name)
-	if screen == null:
-		return false
+	var screen: Control = null
+
+	if p_name != '':
+		screen = get_node(p_name)
+		if screen == null:
+			return false
 
 	if screen == _current_screen_control:
 		return true
@@ -50,21 +88,25 @@ func show_screen(p_name: String, p_info: Dictionary = {}) -> bool:
 	if _current_screen_control:
 		if _current_screen_control.has_method('_hide_screen'):
 			_current_screen_control._hide_screen()
-		_current_screen_control.visible = false
 
-	for child in $Screens.get_children():
+	for child in get_children():
 		child.visible = (screen == child)
 
-	current_screen = screen
+	_current_screen = p_name
+	_current_screen_control = screen
 
-	if screen.has_method("_show_screen"):
+	if screen and screen.has_method("_show_screen"):
 		screen._show_screen(p_info)
 
 	return true
 
 
+func get_current_screen_control() -> Control:
+	return _current_screen_control
+
+
 func _notification(p_what: int) -> void:
 	match p_what:
 		NOTIFICATION_CHILD_ORDER_CHANGED:
-			_update_children()
-
+			if is_node_ready():
+				_update_children()
