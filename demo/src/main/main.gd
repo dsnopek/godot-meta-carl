0c5c0a2fd04dd0121efff030a50915d0d9163406
David Snopek <dsnopek@gmail.com>
2025-08-30 13:17:31 -0500

    Hide hands when OpenXR loses focus

--- demo/src/main/main.gd
+++ demo/src/main/main.gd
@@ -19,3 +19,11 @@ func _on_ui_screen_hidden(p_screen_control: Control) -> void:
 	if p_screen_control.name == 'DefinitionScreen':
 		if playback:
 			playback.visible = false
+
+
+func _on_start_xr_focus_gained() -> void:
+	get_tree().set_group("hand", "visible", true)
+
+
+func _on_start_xr_focus_lost() -> void:
+	get_tree().set_group("hand", "visible", false)
